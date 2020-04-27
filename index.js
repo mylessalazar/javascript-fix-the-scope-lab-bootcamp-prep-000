8a8b426372fbd7693c4d39e4ef947473befb17f7
Myles <mylesssalazar@gmail.com>
2020-04-27 18:34:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,9 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
   // Feel free to move things around!
   const two = 2
+    return n + two
 }
 
 var funkyFunction = function() {
